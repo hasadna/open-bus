5aa999c26f31b625359c9700790c7733fd442090
Matan Grin <matangrn@gmail.com>
2016-04-22 00:32:21 +0300

    Renamed the class Bus to ArrivingBus

--- siri_parser.py
+++ siri_parser.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 
 
-class Bus(object):
+class ArrivingBus(object):
     def __init__(self,
                  line_ref,
                  direction_ref,
@@ -21,7 +21,7 @@ class Bus(object):
         self.stop_point_ref = stop_point_ref
 
     def __str__(self):
-        print("Bus<%s, %s, %s, %s, %s, %s, %s, %s>" %
+        print("ArrivingBus<%s, %s, %s, %s, %s, %s, %s, %s>" %
               self.line_ref,
               self.direction_ref,
               self.published_line_name,
@@ -51,14 +51,14 @@ def _get_tag_text(tag):
     return tag
 
 
-def _bus_node_to_bus_obj(bus_node):
+def _bus_node_to_arrivingbus_obj(bus_node):
     """
         Args:
             bus_node - an object taken from Siri XML
                        ("MonitoredStopVisit") using bs4
 
         Returns:
-            An object of type Bus with all the necessary information
+            An object of type ArrivingBus with all the necessary information
             taken from the given bus_node (bs4) object
     """
 
@@ -69,16 +69,16 @@ def _bus_node_to_bus_obj(bus_node):
     stop_point_ref = bus_node.StopPointRef.getText()
     direction_ref = bus_node.DirectionRef.getText()
     destination_ref = bus_node.DestinationRef.getText()
-    line_ref = _get_tag_text(line.LineRef)
+    line_ref = _get_tag_text(bus_node.LineRef)
 
-    return Bus(line_ref,
-               direction_ref,
-               published_line_name,
-               operator_ref,
-               destination_ref,
-               monitoring_ref,
-               expected_arrival_time,
-               stop_point_ref)
+    return ArrivingBus(line_ref,
+                       direction_ref,
+                       published_line_name,
+                       operator_ref,
+                       destination_ref,
+                       monitoring_ref,
+                       expected_arrival_time,
+                       stop_point_ref)
 
 
 def parse_siri_xml(response_xml):
@@ -87,7 +87,7 @@ def parse_siri_xml(response_xml):
         response_xml - Siri response xml (String)
 
     Returns:
-        List of Bus objects
+        List of ArrivingBus objects
     """
 
     soup = BeautifulSoup(response_xml)
@@ -96,4 +96,4 @@ def parse_siri_xml(response_xml):
     busses = soup.find_all("MonitoredStopVisit")
 
     if busses:
-        return [_bus_node_to_bus_obj(bus) for bus in busses]
+        return [_bus_node_to_arrivingbus_obj(bus) for bus in busses]
