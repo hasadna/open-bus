689a8c536877d4c175c53c7294030d379ee65656
bla@bla.com <bla@bla.com>
2016-04-25 19:58:36 +0300

    Rename ArrivingBus to BusArrival.

--- siri_parser.py
+++ siri_parser.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 
 
-class ArrivingBus(object):
+class BusArrival(object):
     def __init__(self,
                  line_ref,
                  direction_ref,
@@ -21,7 +21,7 @@ class ArrivingBus(object):
         self.stop_point_ref = stop_point_ref
 
     def __str__(self):
-        return "ArrivingBus<%s, %s, %s, %s, %s, %s, %s, %s>" %
+        return "BusArrival<%s, %s, %s, %s, %s, %s, %s, %s>" %
               (self.line_ref,
               self.direction_ref,
               self.published_line_name,
@@ -51,14 +51,14 @@ def _get_tag_text(tag):
     return tag
 
 
-def _bus_node_to_arrivingbus_obj(bus_node):
+def _bus_node_to_busarrival_obj(bus_node):
     """
         Args:
             bus_node - an object taken from Siri XML
                        ("MonitoredStopVisit") using bs4
 
         Returns:
-            An object of type ArrivingBus with all the necessary information
+            An object of type BusArrival with all the necessary information
             taken from the given bus_node (bs4) object
     """
 
@@ -71,7 +71,7 @@ def _bus_node_to_arrivingbus_obj(bus_node):
     destination_ref = bus_node.DestinationRef.getText()
     line_ref = _get_tag_text(bus_node.LineRef)
 
-    return ArrivingBus(line_ref,
+    return BusArrival(line_ref,
                        direction_ref,
                        published_line_name,
                        operator_ref,
@@ -87,7 +87,7 @@ def parse_siri_xml(response_xml):
         response_xml - Siri response xml (String)
 
     Returns:
-        List of ArrivingBus objects
+        List of BusArrival objects
     """
 
     soup = BeautifulSoup(response_xml)
@@ -96,4 +96,4 @@ def parse_siri_xml(response_xml):
     busses = soup.find_all("MonitoredStopVisit")
 
     if busses:
-        return [_bus_node_to_arrivingbus_obj(bus) for bus in busses]
+        return [_bus_node_to_busarrival_obj(bus) for bus in busses]
