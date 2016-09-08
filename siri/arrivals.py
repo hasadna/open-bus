c30c87d98572ac624b27c2b66435880fcc68530b
Daphna Shezaf <daphna@shezaf.com>
2016-09-08 14:51:27 +0100

    updates to fetch_and_store_arrivals: using Open Train as a proxy is turned off by default, can be turned on from command line parameters

--- siri/arrivals.py
+++ siri/arrivals.py
@@ -3,7 +3,7 @@ import pytz
 import traceback
 from jinja2 import Environment, FileSystemLoader
 from datetime import datetime
-
+from http.client import IncompleteRead
 
 REQUEST_TEMPLATE_FILE = "request.template"
 REQUEST_TEMPLATE = Environment(loader=FileSystemLoader('templates')).get_template(REQUEST_TEMPLATE_FILE)
@@ -11,7 +11,8 @@ HEADERS = {'Content-Type': 'text/xml; charset=utf-8'}
 SIRI_SERVICES_URL = 'http://siri.motrealtime.co.il:8081/Siri/SiriServices'
 HTTP_PROXY = 'http://openbus@192.241.154.128:5035'
 
-def get_arrivals_response_xml(request_xml):
+
+def get_arrivals_response_xml(request_xml, use_proxy=False):
     """
     Args:
         request_xml - Siri request XML (String)
@@ -20,13 +21,15 @@ def get_arrivals_response_xml(request_xml):
     """
     try:
         proxy = urllib.request.ProxyHandler({'http': HTTP_PROXY})
-        opener = urllib.request.build_opener(proxy)
-        urllib.request.install_opener(opener)
+        if use_proxy:
+            opener = urllib.request.build_opener(proxy)
+            urllib.request.install_opener(opener)
         req = urllib.request.Request(SIRI_SERVICES_URL, headers=HEADERS, data=request_xml.encode('utf8'))
         res = urllib.request.urlopen(req).read()
         return res
-    except urllib.request.URLError:
-        traceback.print_exc()
+    except IncompleteRead as e:
+        print("http.client.IncompleteRead happened on sending SIRI request:", e)
+        return e.partial
 
 
 def get_arrivals_request_xml(stops):
--- siri/fetch_and_store_arrivals.py
+++ siri/fetch_and_store_arrivals.py
@@ -1,8 +1,6 @@
-# TODO: fetch all stops from gtfs using get_stops
-# TODO: allow quering with custom time ranges
 
 import argparse
-import arrivals, siri_parser, db
+import arrivals, siri_parser, db, time
 
 
 def parse_flags():
@@ -22,14 +20,25 @@ def parse_flags():
                         help="Openbus project DB password")
     parser.add_argument("--stops_file", type=str,
                         help="List of stops to query about, space separated.")
+    parser.add_argument('--use_proxy', type=bool, default=False,
+                        help="Whether to use Open Train server as proxy")
     return parser.parse_args()
 
 
-def fetch_and_store_arrivals(connection_details, stops):
+# def dump_response_to_file(r):
+#     with open("/tmp/siri_response_%s" % time.strftime("%Y%m%d-%H%M%S"), 'wb') as f:
+#         f.write(r)
+
+
+def fetch_and_store_arrivals(connection_details, stops, use_proxy=False):
     conn = db.connect(**connection_details)
     request_xml = arrivals.get_arrivals_request_xml(stops)
-    response_xml = arrivals.get_arrivals_response_xml(request_xml)
+    response_xml = arrivals.get_arrivals_response_xml(request_xml, use_proxy)
+    if "User authentication failed".encode('utf-8') in response_xml:
+        raise Exception("Error connecting to SIRI: user authentication failed")
+    # dump_response_to_file(response_xml)
     parsed_arrivals = siri_parser.parse_siri_xml(response_xml)
+    print("%d arrivals parsed" % len(parsed_arrivals))
     db.insert_arrivals(parsed_arrivals, conn)
     print("Successfully inserted data")
 
@@ -49,7 +58,7 @@ def main():
         "port": args.db_port
     }
     stops = get_stops(args.stops_file)
-    fetch_and_store_arrivals(connection_details=conn, stops=stops)
+    fetch_and_store_arrivals(connection_details=conn, stops=stops, use_proxy=args.use_proxy)
 
 
 if __name__ == '__main__':
