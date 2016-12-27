8c3e2c08a3471d926b32bf3f41d0a31108018e79
Daphna Shezaf <daphna@shezaf.com>
2016-12-27 23:24:11 +0000

    fixed merge issue introduced in previous commit

--- gtfs/bus2train/parse_passengers_trains.py
+++ gtfs/bus2train/parse_passengers_trains.py
@@ -9,7 +9,7 @@ The script does 3 things:
 import csv
 import argparse
 from datetime import datetime
-<<<<<<< HEAD
+<< << << < HEAD
 import io
 
 train_station_to_code = """
@@ -99,40 +99,6 @@ def main(data_path, output_path):
         writer.write_row(["Station", "Date", "Hour", "PassengersCount", "Day"])
         for row in read_data(data_path):
             writer.write_row(row)
-=======
-import calendar
-
-
-def create_dict_of_stations(dict_stations_path):
-    with open(dict_stations_path, mode='r', encoding='utf8') as infile:
-        reader = csv.reader(infile)
-        station_dict = {rows[0]: rows[1] for rows in reader}
-        return station_dict
-
-
-def main(data_path, dict_path,output_path):
-    text = ""
-    station_dict = create_dict_of_stations(dict_path)
-    with open(data_path, 'r', encoding='utf8') as f:
-        reader = csv.reader(f)
-        line =  next(reader, None)  # skip header
-        line = ','.join(map(str, line)) + '\n'
-        text += line
-        for line in reader:
-            if len(line) < 4:  # fix missing values
-                line.append('')
-            if line[0] != "destination_station":
-                line[0] = station_dict[line[0]]  # Change Rakevet Israel names convention to GTFS MOT stop_code
-                day = calendar.day_name[datetime.strptime(line[1], '%d-%m-%Y').weekday()]  # extract day name
-                line.append(day)
-            else:
-                line[0] = 'stop_code'  # change destination_station to stop_code
-                line.append("Day")
-            line = ','.join(map(str, line)) + '\n'
-            text += line
-    with open(output_path, 'w', encoding='utf8') as f:
-        f.write(text)
->>>>>>> origin/bus2train-sql
 
 
 if __name__ == '__main__':
