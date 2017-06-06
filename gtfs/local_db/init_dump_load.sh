7f48cf19aa77c246bc8047cf6d9987c37aef9226
MYank0 <michalyanko1@gmail.com>
2017-06-06 12:19:17 +0300

    fixed an error of login and schema upload

--- gtfs/local_db/init_dump_load.sh
+++ gtfs/local_db/init_dump_load.sh
@@ -4,6 +4,7 @@ psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
     CREATE ROLE obus;
     CREATE DATABASE obus;
     GRANT ALL PRIVILEGES ON DATABASE obus TO obus;
+    ALTER ROLE obus WITH LOGIN;
 
 EOSQL
-pg_restore -h localhost -p 5432 -U postgres -d obus -v /docker-entrypoint-initdb.d/dump.dmp
+pg_restore -h localhost -p 5432 -U postgres -d obus -t gtfs_calendar -t gtfs_routes -t gtfs_shapes -t gtfs_stop_times -t gtfs_stops -t gtfs_trips -t route_story_stops -t siri_arrivals_filtered -t siri_raw_responses -t station_walking_distance -t train_passenger_counts -t train_passengers_per_hour -t train_station_exits -v /docker-entrypoint-initdb.d/dump.dmp
