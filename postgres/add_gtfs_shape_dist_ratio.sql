39b9e4cb9c47695d2b54a294a937acdcdd81a913
Ori Folger <ori.folger@gmail.com>
2017-06-06 12:42:45 +0300

    Create add_gtfs_shape_distro_ratio.sql

---
+++ postgres/add_gtfs_shape_dist_ratio.sql
@@ -0,0 +1,7 @@
+ALTER TABLE gtfs_stop_times ADD COLUMN shape_dist_ratio REAL;
+
+UPDATE gtfs_stop_times
+SET shape_dist_ratio = (1.0*shape_dist_traveled) /
+  (SELECT MAX(shape_dist_traveled)
+   FROM gtfs_stop_times a
+   WHERE a.trip_id = gtfs_stop_times.trip_id);
