c952dba269c61856284b60b9b3f5070d962543fc
MYank0 <michalyanko1@gmail.com>
2017-06-09 18:16:23 +0300

    Add SHAPE_DIST_RATIO field to gtfs_stop_times

--- postgres/insert_gtfs.sql
+++ postgres/insert_gtfs.sql
@@ -218,6 +218,15 @@ CREATE INDEX stop_times_trip_id
 --   (stop_sequence );
 
 
+-- Calculate the ratio of distance that was traveled. See issue #33 for more information
+ALTER TABLE gtfs_stop_times ADD COLUMN shape_dist_ratio REAL;
+
+UPDATE gtfs_stop_times
+SET shape_dist_ratio = (1.0*shape_dist_traveled) /
+  (SELECT MAX(shape_dist_traveled)
+   FROM gtfs_stop_times a
+WHERE a.trip_id = gtfs_stop_times.trip_id);
+
 -- shapes --
 \echo ********** importing shapes **********
 
