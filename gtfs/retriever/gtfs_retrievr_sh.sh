840d5c6b73456690644371f6c4b9dce281ed1c2c
efratoio <efrat.abramovitz@gmail.com>
2017-07-09 21:01:49 +0300

    issue #35 fix

---
+++ gtfs/retriever/gtfs_retrievr_sh.sh
@@ -0,0 +1,18 @@
+
+MOTS_URL=ftp://gtfs.mot.gov.il
+FILE_NAME=israel-public-transportation.zip
+GTFS_DATA_DIR=$1 '#/srv/gtfs_archive'
+LOG_FILE=$2 '#/var/log/gtfs_archive.log'
+DATE=`date "+%Y%m%d"`
+ARCHIVE_FILE=$GTFS_DATA_DIR/$DATE.zip
+
+touch $LOG_FILE
+touch $ARCHIVE_FILE
+
+chmod +x $LOG_FILE
+chmod +x $ARCHIVE_FILE
+
+exec 3>&1 1>>${LOG_FILE} 2>&1
+
+echo -e Getting zip file from $MOTS_URL to $ARCHIVE_FILE \n | tee /dev/fd/3
+wget -O $ARCHIVE_FILE $MOTS_URL/$FILE_NAME
