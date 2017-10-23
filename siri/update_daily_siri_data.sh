6ee37e4196727e2c208168bab2f9f517904fc49f
Michal <michalyanko1@gmail.com>
2017-10-23 23:55:01 +0300

    upgrade daily script - upload gtfs daily

--- siri/update_daily_siri_data.sh
+++ siri/update_daily_siri_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#This script will be used temporary for uploading daily the gtfs and updating siri data
 
 if [ "$#" -ne 2 ]; then
     echo "Usage: update_daily_siri_data.sh db_username db_password"
@@ -14,8 +15,26 @@ year="$(date +'%Y')"
 
 echo updating for date: $day'-'$month'-'$year
 
-psql -h 127.0.0.1 -U $1 -d obus -v v1=$day -v v2=$month -v v3=$year -f ../postgres/adding_trip_id_to_siri_from_gtfs.sql
+gtfs_file='/tmp/gtfs_'$year'-'$month'-'$day'.zip'
+aws s3 cp s3://s3.obus.hasadna.org.il/$year-$month-$day.zip $gtfs_file
+
+PATH=$HOME/open-bus/postgres:$PATH
+cd $HOME/open-bus/postgres
+
+insert_gtfs.sh $gtfs_file $1 $2
+rm $gtfs_file
+echo uploaded gtfs file
+
+psql -h 127.0.0.1 -U $1 -d obus -f add_gtfs_shape_dist_ratio.sql
+echo added gtfs_shape_dist_ratio
+
+psql -h 127.0.0.1 -U $1 -d obus -f create_gtfs_shape_lines_table.sql
+echo created gtfs_shape_lines
+
+psql -h 127.0.0.1 -U $1 -d obus -v v1=$day -v v2=$month -v v3=$year -f adding_trip_id_to_siri_from_gtfs.sql
 echo added trip id
 
-psql -h 127.0.0.1 -U $1 -d obus -v v1=$day -v v2=$month -v v3=$year -f ../postgres/add_route_offset.sql
+psql -h 127.0.0.1 -U $1 -d obus -v v1=$day -v v2=$month -v v3=$year -f add_route_offset.sql
 echo added route offset
+
+#add here script when bus arrived
