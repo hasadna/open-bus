6dfff91279404328ae98f393b374766d776a2e16
LiorDL <liork.dagan@gmail.com>
2018-05-02 22:27:36 +0300

    add documentation

--- siri/update_daily_siri_data.sh
+++ siri/update_daily_siri_data.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-#This script will be used temporary for uploading daily the gtfs and updating siri data
+#This script will be used temporary for uploading daily the gtfs and updating siri_real_time_arrivals table (our evaluation of the arrival time).
+# NOTE - this script doesn't query siri! To make siri request use fetch_and_store_arrivals.py and see working_with_SIRI.md
+# updaing gtfs is done using the insert_gtfs.sh script.
+# updating the siri_real_time_arrivals is based on the analyseRealTime/Main.py code. 
+# analyseRealTime/Main.py relies on existence of:   
+# 1. trip id on each siri entry in the siri_arrivals table (this is not something we get automatically and is done using the adding_trip_id_to_siri_from_gtfs.sql)
+# 2. route offset on each siri entry - done using add_route_offset.sql
+# The trip id to siri entry matching can be done only after adding the following data:
+# 1. add_gtfs_shape_dist_ratio.sql - add to gtfs_stop_times the ratio of the travel distance.
+# 2. create_gtfs_shape_lines_table.sql - turns shape table entries into postgis shapes.
+
 
 if [ "$#" -ne 4 ]; then
     echo "Usage: update_daily_siri_data.sh db_username db_password aws_bucket analysis_config"
