591f0902d37bee90d8485068d82bd78191fc3964
cjer <9987721+cjer@users.noreply.github.com>
2018-07-22 23:43:41 +0300

    add initial tests

---
+++ gtfs_utils/tests/test_gtfs_stats.py
@@ -0,0 +1,7 @@
+import datetime
+from gtfs_utils.gtfs_stats import parse_date
+
+def test_parse_date():
+    date, date_str = parse_date('2099-01-01.zip')
+    assert date == datetime.date(2099, 1, 1)
+    assert date_str == '2099-01-01'
