68a44743739b0969f9f18fdb6ff3868916518c93
Ofir Steinherz <ofirst99@gmail.com>
2018-12-03 22:46:46 +0200

    Change var  to

--- complains/frontend/script.js
+++ complains/frontend/script.js
@@ -1,6 +1,6 @@
-var mymap = L.map('mapid').setView([32.073608, 34.790128], 18);
+var map = L.map('mapid').setView([32.073608, 34.790128], 18);
 
-mymap.scrollWheelZoom.disable();
+map.scrollWheelZoom.disable();
 
 // TODO: Create new `access_token`
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -9,7 +9,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox.streets'
-}).addTo(mymap);
+}).addTo(map);
 
 var busStationIcon = L.icon({
     iconUrl: 'bus_station_icon.svg',
@@ -18,4 +18,4 @@ var busStationIcon = L.icon({
     // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor TODO: check this value after adding `onClick` fun
 });
 
-L.marker([32.073539, 34.789106], {icon: busStationIcon}).addTo(mymap);
+L.marker([32.073539, 34.789106], {icon: busStationIcon}).addTo(map);
