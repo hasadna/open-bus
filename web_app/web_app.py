40d4be6d924bfa96c88d083eaed4dfe7ab1d6b42
GipsyBeggar <omer.big@gmail.com>
2019-03-30 17:05:58 +0300

    final commit

--- web_app/web_app.py
+++ web_app/web_app.py
@@ -1,37 +1,9 @@
-
-import json, urllib.request
+import json, urllib.request, time
 from collections import defaultdict
+from datetime import datetime
 
-print("This line will be printed.")
-
-#18/3/2019: http://142.93.111.211:3000/trips/114 <-> http://142.93.111.211:3000/trips/167
-api_url = "http://142.93.111.211:3000/trips/114"
-req = urllib.request.Request(api_url)
-url = urllib.request.urlopen(api_url).read()
-data_from_json = json.loads(url.decode())
-
-
-# regroup data
-# d = defaultdict(list)
-d = dict()
-
-d["tripId"] = (data_from_json["tripId"])
-d["planned_time"] = (data_from_json["planned_time"])
-d["date"] = (data_from_json["date"])
-d["routeId"] = (data_from_json["routeId"])
-d["routeShortName"] = (data_from_json["routeShortName"])
-d["routeLongName"] = (data_from_json["routeLongName"])
-d["agencyName"] = (data_from_json["agencyName"])
-d["routeType"] = (data_from_json["routeType"])
-print(d)
-
-# del data_from_json["stops"]
-# del data_from_json["shape"]
-# del data_from_json["siri"]
-# data_from_json.pop('stops', None)
-# data_from_json.pop('shape', None)
-# data_from_json.pop('siri', None)
-print(data_from_json)
+dict_output = defaultdict(lambda: defaultdict(int))
+lateness_interval = 300 #seconds
 
 
 def clean_data(json_object):
@@ -43,3 +15,50 @@ def clean_data(json_object):
         print(element1)
     return
 
+
+def open_json(url):
+    url = urllib.request.urlopen(url).read()
+    data_from_json = json.loads(url.decode())
+    return data_from_json
+
+
+def check_depart_on_time(json_object):
+    p = '%H:%M:%S'
+    epoch = datetime(1970, 1, 1)
+
+    planned_time = (data_from_json["planned_time"])
+    # print(planned_time)
+    planned_time_epoch = (datetime.strptime(planned_time, p) - epoch).total_seconds()
+    # print(planned_time_epoch)
+
+    try:
+        time_recorded = (data_from_json["siri"]["features"][0]["properties"]["time_recorded"])
+    except IndexError:
+        return None
+    # print(time_recorded)
+    time_recorded_epoch = (datetime.strptime(time_recorded, p) - epoch).total_seconds()
+    # print(time_recorded_epoch)
+    seconds_elapsed = abs(planned_time_epoch - time_recorded_epoch)
+    # print(seconds_elapsed)
+
+    return lateness_interval > seconds_elapsed
+
+
+if __name__ == '__main__':
+    #18/3/2019: http://142.93.111.211:3000/trips/114 <-> http://142.93.111.211:3000/trips/167
+    #total entries: http://142.93.111.211:3000/trips/0 <-> http://142.93.111.211:3000/trips/1329
+    for i in range(1330):
+        api_url = "http://142.93.111.211:3000/trips/"+str(i)
+        print("checking: " + api_url)
+
+        data_from_json = open_json(api_url)
+        ride_on_time = check_depart_on_time(data_from_json)
+        ride_date = data_from_json["date"]
+        if ride_on_time is not None:
+            dict_output[ride_date]["ride_on_time"] += ride_on_time
+            dict_output[ride_date]["number_of_rides"] += 1
+        else:
+            print("ERROR: problem with data on "+api_url)
+
+    # print(dict_output)
+    json.dump(dict_output, open('results.json', 'w', encoding="utf8"), ensure_ascii=False)
