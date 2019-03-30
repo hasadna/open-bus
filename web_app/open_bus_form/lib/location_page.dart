1b43d6490b6e9b4eed0613baade724bbe281fb05
tomerbenrachel <tomer.ben-rachel@ironsrc.com>
2019-03-30 13:03:59 +0300

    issue-139: Logic to ask user for the location permission at runtime

--- web_app/open_bus_form/lib/location_page.dart
+++ web_app/open_bus_form/lib/location_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_permissions/simple_permissions.dart';
 
 
 class LocationPage extends StatefulWidget {
@@ -11,22 +12,54 @@ class LocationPage extends StatefulWidget {
 
 class _LocationPageState extends State<LocationPage> {
 
-  void requestLocationPermission() {
+  void gatherAndsendUserLocation() {
+    
+  }
 
+  void requestLocationPermission() async {
+    bool locationPermissionStatus = await SimplePermissions.checkPermission(Permission.AccessFineLocation);
+    print("Location Permission status is " + locationPermissionStatus.toString());
+    if (!locationPermissionStatus) {
+      PermissionStatus requestPermissionStatus = await SimplePermissions.requestPermission(Permission.AccessFineLocation);
+      switch (requestPermissionStatus) {
+        case PermissionStatus.authorized : {
+          gatherAndsendUserLocation();
+          break;
+        }
+        case PermissionStatus.denied : {
+          print("User denied location permission");
+          //TODO ask again?
+          break;
+        }
+        case PermissionStatus.notDetermined : {
+          print("Permission status " + requestPermissionStatus.toString());
+          //TODO figure out what to do with status not determined (ask again?)
+          break;
+        }
+        case PermissionStatus.deniedNeverAsk:
+        case PermissionStatus.restricted : {
+          //TODO consider closing the application?
+          break;
+        }
+        default : {
+          print("Unknown permission status " + requestPermissionStatus.toString());
+        }
+      }
+
+    }
   }
 
   @override
   void initState() {
     super.initState();
     requestLocationPermission();
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login To Open Bus Form'),
+        title: Text('Requesting Location Permission'),
       ),
       body:Column(
       crossAxisAlignment: CrossAxisAlignment.center,
