b5938dc73d6ce79cbe21b40a2ae5a82d096e5d13
Or Bin <orbin50@gmail.com>
2019-05-13 21:55:48 +0300

    #188 Removed BUCKET_VALID_FILES_RE from gtfs_stats_conf

--- gtfs/gtfs_utils/gtfs_utils/gtfs_stats_conf.py
+++ gtfs/gtfs_utils/gtfs_utils/gtfs_stats_conf.py
@@ -3,7 +3,6 @@ import re
 OUTPUT_FILE_NAME_RE = re.compile('^(?P<date_str>[^_]+?)'
                                  '_(?P<type>\w+)\.pkl\.gz')
 
-BUCKET_NAME = 's3.obus.hasadna.org.il'
 BUCKET_VALID_FILES_RE = re.compile('2019-\d\d-\d\d\.zip')
 #BUCKET_VALID_FILES_RE = re.compile('2019-03-12.zip')
 
