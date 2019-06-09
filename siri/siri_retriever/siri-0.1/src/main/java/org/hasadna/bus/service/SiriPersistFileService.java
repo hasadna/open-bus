1d695af8930682fd170e84eb7d04ca121bd2cdd1
Evyatar <evyatar@tikalk.com>
2019-06-09 16:10:36 +0300

    add sending to external system

--- siri/siri_retriever/siri-0.1/src/main/java/org/hasadna/bus/service/SiriPersistFileService.java
+++ siri/siri_retriever/siri-0.1/src/main/java/org/hasadna/bus/service/SiriPersistFileService.java
@@ -2,7 +2,12 @@ package org.hasadna.bus.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 @Component
 public class SiriPersistFileService implements SiriPersistService {
@@ -12,10 +17,32 @@ public class SiriPersistFileService implements SiriPersistService {
     // this logger should be defined to a specific file - see logback.xml
     protected final Logger fileLogger = LoggerFactory.getLogger("SiriRealTimeData");
 
+    @Value("${external.send:false}")
+    boolean sendExternal;
+
+    @Value("${external.url}")
+    String url;
+
+    @Autowired
+    RestTemplateBuilder restTemplateBuilder;
+
     @Override
     public void persistShortSummary(String summary) {
         logger.trace("persisting...");
         fileLogger.info(summary);
+        sendToExternal(summary);
         logger.trace("          ...Done");
+    }
+
+    private void sendToExternal(String str) {
+        try {
+            if (sendExternal && !StringUtils.isEmpty(str)) {
+                restTemplateBuilder.build().postForEntity(url, new HttpEntity<String>(str, null), String.class);
+            }
+        }
+        catch (Exception ex) {
+            logger.debug("absorbing ", ex);
+        }
+
     }
 }
