86cd9ddaee154525e8e7f6e013586a1dbcca6b9d
Aviv Sela <avivsela10@gmail.com>
2019-09-11 07:28:50 +0300

    add initial class for firebase crud

---
+++ firebase/backend/firebase_crud.py
@@ -0,0 +1,23 @@
+from google.cloud import firestore
+
+
+class BaseCrud:
+    def __init__(self, connection: firestore.Client, collection_name: str):
+        self.connection = connection
+        self.collection_name = collection_name
+
+    def create(self, content, doc_id=None):
+        self.connection.collection(self.collection_name).document(document_id=doc_id).create(content)
+
+
+class SiriRideCrud(BaseCrud):
+
+    def __init__(self, connection):
+        super().__init__(connection, collection_name='test')
+
+    def create(self, siri_ride, doc_id=None):
+        super().create(siri_ride, doc_id=doc_id)
+
+
+SiriRideCrud(firestore.Client(project='openbus')).create({'foo': 23, 'bar': 45})
+
