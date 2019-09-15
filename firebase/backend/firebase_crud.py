58875631b6451f9ab7547d0c3b2bc0275b486ffc
Aviv Sela <avivsela10@gmail.com>
2019-09-15 23:41:06 +0300

    add tests

--- firebase/backend/firebase_crud.py
+++ firebase/backend/firebase_crud.py
@@ -1,25 +1,53 @@
+import itertools
 from google.cloud import firestore
+from multiprocessing.dummy import Pool as ThreadPool
+from multiprocessing import cpu_count
 
-# make sure you have GOOGLE_APPLICATION_CREDENTIALS environment variable pointing to service account credentials file
-# export GOOGLE_APPLICATION_CREDENTIALS="path/to/file"
+# Max 500
+NUMBER_OF_ITEMS_IN_ITERABLE_BATCH = 500
+NUMBER_OF_ITEMS_IN_PARALLEL_BATCH = 200
+
 
 class BaseCrud:
     def __init__(self, connection: firestore.Client, collection_name: str):
         self.connection = connection
         self.collection_name = collection_name
 
+    def _collection_ref(self, doc_id=None):
+        return self.connection.collection(self.collection_name).document(document_id=doc_id)
+
     def create(self, content, doc_id=None):
-        self.connection.collection(self.collection_name).document(document_id=doc_id).create(content)
+        self._collection_ref(doc_id=doc_id).create(content)
+
+    def read(self, doc_id):
+        return self._collection_ref(doc_id).get()
+
+    def create_multi(self, contents, parallel=False):
+        if not parallel:
+            for chunk_of_rides in grouper(NUMBER_OF_ITEMS_IN_ITERABLE_BATCH, contents):
+                self._batch_commit(chunk_of_rides)
+            return
+        self._create_multi_parallel(contents, num_threads=cpu_count())
+
+    def _batch_commit(self, chunk):
+        batch = self.connection.batch()
+        coll = self.connection.collection(self.collection_name)
+        for itm in chunk:
+            batch.create(coll.document(), itm)
+        batch.commit()
+
+    # 187 sec for creating 12,000 documents with size of a SiriRide doc
+    def _create_multi_parallel(self, contents, num_threads):
+        pool = ThreadPool(num_threads)
+        pool.map(self._batch_commit, grouper(NUMBER_OF_ITEMS_IN_PARALLEL_BATCH, contents))
+        pool.close()
+        pool.join()
 
 
-class SiriRideCrud(BaseCrud):
-
-    def __init__(self, connection):
-        super().__init__(connection, collection_name='test')
-
-    def create(self, siri_ride, doc_id=None):
-        super().create(siri_ride, doc_id=doc_id)
-
-
-SiriRideCrud(firestore.Client(project='openbus')).create({'foo': 23, 'bar': 45})
-
+def grouper(n, iterable):
+    it = iter(iterable)
+    while True:
+        chunk = tuple(itertools.islice(it, n))
+        if not chunk:
+            return
+        yield chunk
---
+++ firebase/backend/test_baseCrud.py
@@ -0,0 +1,70 @@
+from unittest import TestCase
+from unittest.mock import MagicMock
+from unittest.mock import call
+from firebase_crud import BaseCrud, grouper
+
+
+# noinspection PyTypeChecker
+class TestBaseCrud(TestCase):
+
+    def test__collection_ref(self):
+        # Arrange
+
+        expected_doc_id = 'foo'
+        expected_collection_name = 'bar'
+        mock_conn = MagicMock()
+
+        crud = BaseCrud(mock_conn, collection_name=expected_collection_name)
+
+        # Act
+        crud._collection_ref(doc_id=expected_doc_id)
+
+        # Assert
+        expected_conn_call_list = call.collection(expected_collection_name).document(document_id=expected_doc_id)\
+            .call_list()
+
+        self.assertEqual(expected_conn_call_list, mock_conn.mock_calls)
+
+    def test_create(self):
+        # Arrange
+        expected_doc_id = 'foo'
+        expected_content = {}
+        mock_conn = MagicMock()
+
+        crud = BaseCrud(None, None)
+        crud._collection_ref = mock_conn
+
+        # Expected
+        expected_conn_call_list = call(doc_id=expected_doc_id).create(expected_content).call_list()
+
+        # Act
+        crud.create(content=expected_content, doc_id=expected_doc_id)
+
+        # Assert
+        self.assertEqual(expected_conn_call_list, mock_conn.mock_calls)
+
+    def test_read(self):
+        # Arrange
+        doc_id = 'foo'
+        crud = BaseCrud(None, None)
+        crud._collection_ref = MagicMock()
+
+        # Expect
+        expected_conn_call_list = call(doc_id).get().call_list()
+
+        # Act
+        crud.read(doc_id=doc_id)
+
+        # Assert
+        self.assertEqual(expected_conn_call_list, crud._collection_ref.mock_calls)
+
+
+    def test_grouper(self):
+        # Expect
+        expected = [(0, 1), (2,)]
+
+        # Act
+        actual = [i for i in grouper(2, range(3))]
+
+        # Assert
+        self.assertEqual(expected, actual)
