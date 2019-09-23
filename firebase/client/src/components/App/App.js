8981f121926adc774508bc9d82b13f477c33ea15
HillaShx <hilla.sh@gmail.com>
2019-09-23 22:01:29 +0300

    added navigation controls with dummy content, started seperating the main view components

--- firebase/client/src/components/App/App.js
+++ firebase/client/src/components/App/App.js
@@ -9,6 +9,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const mapStateToProps = (state) => {
   return { 
@@ -17,26 +18,66 @@ const mapStateToProps = (state) => {
   };
 };
 
+function RideQuery(props) {
+  return (
+  <Grid container className="main-grid" spacing={0} justify="center">
+    <Grid item className="main-grid-item-ride-selector" xs={12}>
+      <RiderSelector />
+    </Grid>
+    <Grid item className="main-grid-item-ride-viewer" xs={12} align="center">
+      <RideViewer isLoading={props.isFetchingRideDate} routeData={props.rideDate} />
+    </Grid>
+  </Grid>
+  )
+}
+
+function About() {
+  return <h2>This is About</h2>
+}
+function Users() {
+  return <h2>This is Users</h2>
+}
 function App(props) {
   return (
+    
     <>
-      <CssBaseline />
-      <div className="app">
-        <AppBar position="static">
+       <CssBaseline />
+       <div className="app">
+        <Router>
+         <AppBar position="static">
           <Toolbar>
-            <Typography variant="h5">OpenBus</Typography>
-          </Toolbar>
-        </AppBar>
-        <Grid container className="main-grid" spacing={0} justify="center">
-          <Grid item className="main-grid-item-ride-selector" xs={12}>
-            <RiderSelector />
-          </Grid>
-          <Grid item className="main-grid-item-ride-viewer" xs={12} align="center">
-            <RideViewer isLoading={props.isFetchingRideDate} routeData={props.rideDate} />
-          </Grid>
-        </Grid>
-      </div>
-    </>
+              <Typography variant="h5">OpenBus</Typography>
+              <div>
+                <nav>
+                  <ul>
+                    <li>
+                      <Link to="/">Ride Query</Link>
+                    </li>
+                    <li>
+                      <Link to="/about/">About</Link>
+                    </li>
+                    <li>
+                      <Link to="/users/">Users</Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+           </Toolbar>
+         </AppBar>
+          <Route path="/" exact component={RideQuery} />
+          <Route path="/about/" component={About} />
+          <Route path="/users/" component={Users} />
+        </Router>        
+         {/* <Grid container className="main-grid" spacing={0} justify="center">
+           <Grid item className="main-grid-item-ride-selector" xs={12}>
+             <RiderSelector />
+           </Grid>
+           <Grid item className="main-grid-item-ride-viewer" xs={12} align="center">
+             <RideViewer isLoading={props.isFetchingRideDate} routeData={props.rideDate} />
+           </Grid>
+         </Grid> */}
+       </div>
+     </>
   );
 }
 
---
+++ firebase/client/src/components/RideQuery/RideQuery.js
@@ -0,0 +1,16 @@
+// import React from "react";
+
+// function RideQuery(props) {
+//   return (
+//   <Grid container className="main-grid" spacing={0} justify="center">
+//     <Grid item className="main-grid-item-ride-selector" xs={12}>
+//       <RiderSelector />
+//     </Grid>
+//     <Grid item className="main-grid-item-ride-viewer" xs={12} align="center">
+//       <RideViewer isLoading={props.isFetchingRideDate} routeData={props.rideDate} />
+//     </Grid>
+//   </Grid>
+//   )
+// }
+
+// export 
