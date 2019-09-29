e259d4b01a82f7ea7c8b7ae3c50f7ddd826f4f9f
Aviv Sela <avivsela10@gmail.com>
2019-09-29 15:36:30 +0300

    add tests

--- firebase/backend/firebase_crud.py
+++ firebase/backend/firebase_crud.py
@@ -2,19 +2,18 @@ import itertools
 from google.cloud import firestore
 from multiprocessing.dummy import Pool as ThreadPool
 from multiprocessing import cpu_count
-from typing import Iterator, Dict
+from typing import Iterator
 
 from firebase_documents import FirebaseDocument
-
-# Max 500
-NUMBER_OF_ITEMS_IN_ITERABLE_BATCH = 500
-NUMBER_OF_ITEMS_IN_PARALLEL_BATCH = 200
 
 # make sure you have GOOGLE_APPLICATION_CREDENTIALS environment variable pointing to service account credentials file
 # export GOOGLE_APPLICATION_CREDENTIALS="path/to/file"
 
 
 class BaseCrud:
+    NUMBER_OF_ITEMS_IN_SEQUENTIAL_BATCH = 500
+    NUMBER_OF_ITEMS_IN_PARALLEL_BATCH = 200
+
     def __init__(self, connection: firestore.Client, collection_name: str):
         self.connection = connection
         self.collection_name = collection_name
@@ -30,24 +29,28 @@ class BaseCrud:
     def read(self, doc_id):
         return self._create_document_ref(doc_id).get()
 
-    def create_multi(self, contents: Iterator[Dict[None, None]], parallel=False):
-        if not parallel:
-            for chunk_of_rides in grouper(NUMBER_OF_ITEMS_IN_ITERABLE_BATCH, contents):
-                self._batch_commit(chunk_of_rides)
-            return
-        self._create_multi_parallel(contents, num_threads=cpu_count())
+    def create_multi(self, contents: Iterator[FirebaseDocument], parallel=False):
+        if parallel:
+            self._create_multi_parallel(contents, num_threads=cpu_count())
+        else:
+            self._create_multi_sequential(contents)
 
     def _batch_commit(self, chunk: Iterator[FirebaseDocument]):
         batch = self.connection.batch()
         coll = self.connection.collection(self.collection_name)
+
         for itm in chunk:
             batch.create(coll.document(itm.get_firebase_id()), itm.to_firebase_dict())
         batch.commit()
 
+    def _create_multi_sequential(self, contents: Iterator[FirebaseDocument]):
+        for chunk_of_rides in grouper(self.NUMBER_OF_ITEMS_IN_SEQUENTIAL_BATCH, contents):
+            self._batch_commit(chunk_of_rides)
+
     # 187 sec for creating 12,000 documents with size of a SiriRide doc
-    def _create_multi_parallel(self, contents, num_threads):
+    def _create_multi_parallel(self, contents: Iterator[FirebaseDocument], num_threads):
         pool = ThreadPool(num_threads)
-        pool.map(self._batch_commit, grouper(NUMBER_OF_ITEMS_IN_PARALLEL_BATCH, contents))
+        pool.map(self._batch_commit, grouper(self.NUMBER_OF_ITEMS_IN_PARALLEL_BATCH, contents))
         pool.close()
         pool.join()
 
--- firebase/backend/test_baseCrud.py
+++ firebase/backend/test_baseCrud.py
@@ -7,8 +7,11 @@ from firebase_crud import BaseCrud, grouper
 from firebase_documents import Point
 
 
-# noinspection PyTypeChecker
+# noinspection PyTypeChecker,PyUnresolvedReferences
 class TestBaseCrud(TestCase):
+
+    def setUp(self):
+        pass
 
     def test__collection_ref(self):
         # Arrange
@@ -89,6 +92,89 @@ class TestBaseCrud(TestCase):
 
         # Assert
         self.assertEqual(expected, actual)
+
+    def test_create_multi_calls__create_multi_sequential(self):
+        # Arrange
+        crud = BaseCrud(None, None)
+        crud._create_multi_sequential = MagicMock()
+
+        # Act
+        crud.create_multi([{}], parallel=False)
+
+        # Assert
+        self.assertTrue(crud._create_multi_sequential.called)
+
+    def test_create_multi_calls__create_multi_sequential_as_default(self):
+        # Arrange
+        crud = BaseCrud(None, None)
+        crud._create_multi_sequential = MagicMock()
+
+        # Act
+        crud.create_multi([{}])
+
+        # Assert
+        self.assertTrue(crud._create_multi_sequential.called)
+
+    def test_create_multi_calls__create_multi_parallel(self):
+        # Arrange
+        crud = BaseCrud(None, None)
+        crud._create_multi_parallel = MagicMock()
+
+        # Act
+        crud.create_multi([{}], parallel=True)
+
+        # Assert
+        self.assertTrue(crud._create_multi_parallel.called)
+
+    def test__batch_commit_calls_to_conn_batch(self):
+        # Arrange
+        # mock conn.collection
+        collection_ref = MagicMock()
+        collection_ref.document = lambda x: ('doc_ref', x)
+        conn = MagicMock()
+        conn.collection = lambda x: collection_ref
+        # mock conn.batch
+        batch_session = MagicMock()
+        batch_session_create_method = MagicMock()
+        batch_session.create = batch_session_create_method
+        conn.batch = lambda: batch_session
+
+        crud = BaseCrud(conn, None)
+
+        doc = Point(1.2, 2.1)
+        doc.to_firebase_dict = lambda: dict(foo='bar')
+        expected_doc_ref = ('doc_ref', doc.get_firebase_id())
+
+        # Act
+
+        crud._batch_commit([doc])
+
+        # Assert
+        batch_session_create_method.assert_called_with(expected_doc_ref, doc.to_firebase_dict())
+
+    def test_create_multi_sequential__calls_to__batch_commit(self):
+        # Arrange
+        crud = BaseCrud(None, None)
+        _batch_commit_method = MagicMock()
+        crud._batch_commit = _batch_commit_method
+        expected_calls = 8
+        docs = [Point(1.1, 1.1) for _ in range(crud.NUMBER_OF_ITEMS_IN_SEQUENTIAL_BATCH * expected_calls)]
+        # Act
+        crud._create_multi_sequential(docs)
+
+        self.assertEqual(expected_calls, _batch_commit_method.call_count)
+
+    def test_create_multi_parallel__calls_to__batch_commit(self):
+        # Arrange
+        crud = BaseCrud(None, None)
+        _batch_commit_method = MagicMock()
+        crud._batch_commit = _batch_commit_method
+        expected_calls = 8
+        docs = [Point(1.1, 1.1) for _ in range(crud.NUMBER_OF_ITEMS_IN_PARALLEL_BATCH * expected_calls)]
+        # Act
+        crud._create_multi_parallel(docs, 4)
+
+        self.assertEqual(expected_calls, _batch_commit_method.call_count)
 
 
 if __name__ == '__main__':
