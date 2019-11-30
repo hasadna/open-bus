de4db5973a867d3fd0c6738311c518a2cd50efc7
TheFrok <yottam@gmail.com>
2019-11-30 18:23:22 +0200

    Fixed the side way scrolling problem by adding a static css

--- gtfs/docs/source/conf.py
+++ gtfs/docs/source/conf.py
@@ -94,6 +94,14 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# I used this guide to override the Read The Docs table width problem in css:
+# https://rackerlabs.github.io/docs-rackspace/tools/rtd-tables.html
+html_context = {
+    'css_files': [
+        '_static/theme_overrides.css',  # override wide tables in RTD theme
+        ],
+     }
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
