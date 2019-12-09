3c57ca71040748bbb4d0d32ca20bf128292d012e
TheFrok <yottam@gmail.com>
2019-12-09 15:20:41 +0200

    Check all keys not just objects

--- gtfs/gtfs_utils/tests/test_configuration.py
+++ gtfs/gtfs_utils/tests/test_configuration.py
@@ -3,7 +3,7 @@ import os
 
 from ..gtfs_utils.configuration import validate_configuration_schema, get_json_schema
 
-DESCRIPTION_KEYS = ('title', 'description', '$$description', '$$title')
+DESCRIPTION_KEYS = ('title', 'description', '$$description', '$$title', '$ref')
 
 
 def test_example_config():
@@ -13,16 +13,15 @@ def test_example_config():
 
 
 def validate_description(key_name, schema_item):
-    if 'type' in schema_item:
-        assert not set(DESCRIPTION_KEYS).isdisjoint(schema_item.keys()), \
-                f'{key_name!r} key in the JSON Schema has no description'
-        if schema_item['type'] == 'object':
-            assert 'properties' in schema_item
-            for key, value in schema_item['properties'].items():
-                validate_description(key, value)
-        if 'definitions' in schema_item:
-            for key, value in schema_item['definitions'].items():
-                validate_description(key, value)
+    assert not set(DESCRIPTION_KEYS).isdisjoint(schema_item.keys()), \
+            f'{key_name!r} key in the JSON Schema has no description'
+    if 'type' in schema_item and schema_item['type'] == 'object':
+        assert 'properties' in schema_item
+        for key, value in schema_item['properties'].items():
+            validate_description(key, value)
+    if 'definitions' in schema_item:
+        for key, value in schema_item['definitions'].items():
+            validate_description(key, value)
 
 
 def test_configuration_documentation():
