4ba13d84903929d4cb57d954000d17da51511a68
Evyatar <evyatar@tikalk.com>
2020-06-05 14:19:58 +0300

    fix removal of soap envelope from Response

--- siri/siri_retriever/siri-0.1/src/main/java/org/hasadna/bus/util/SoapUtils.java
+++ siri/siri_retriever/siri-0.1/src/main/java/org/hasadna/bus/util/SoapUtils.java
@@ -5,7 +5,18 @@ public class SoapUtils {
     public static String removeSoapEnvelope(String content) {
         // remove soap envelope (ugly)
         content = content.trim();   // remove whitespaces, including newlines in the end of line
-        final String prefix = "<?xml version='1.0' encoding='UTF-8'?><S:Envelope xmlns:S=\"http://schemas.xmlsoap.org/soap/envelope/\"><S:Body>";
+        final String prefix1 = "<?xml version='1.0' encoding='UTF-8'?>";
+        final String prefix2 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+        if (content.startsWith(prefix1)) {
+            content = content.substring(prefix1.length());
+        }
+        else if (content.startsWith(prefix2)) {
+            content = content.substring(prefix2.length());
+        }
+        else {
+            // TODO log
+        }
+        final String prefix = "<S:Envelope xmlns:S=\"http://schemas.xmlsoap.org/soap/envelope/\"><S:Body>";
         final String suffix = "</S:Body></S:Envelope>";
         if (content.startsWith(prefix)) {
             content = content.substring(prefix.length());
