ffcb03b21356c431b0b9527b92706dfd2eb34ea3
Evyatar <evyatar@tikalk.com>
2020-06-05 14:32:48 +0300

    fix removal of soap envelope from Response

--- siri/siri_retriever/siri-0.1/src/main/java/org/hasadna/bus/util/SoapUtils.java
+++ siri/siri_retriever/siri-0.1/src/main/java/org/hasadna/bus/util/SoapUtils.java
@@ -1,8 +1,14 @@
 package org.hasadna.bus.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SoapUtils {
 
+    final static Logger logger = LoggerFactory.getLogger(SoapUtils.class);
+
     public static String removeSoapEnvelope(String content) {
+        logger.trace("enter removeSoapEnvelope, content={}",content);
         // remove soap envelope (ugly)
         content = content.trim();   // remove whitespaces, including newlines in the end of line
         final String prefix1 = "<?xml version='1.0' encoding='UTF-8'?>";
@@ -16,14 +22,19 @@ public class SoapUtils {
         else {
             // TODO log
         }
+        //<?xml version="1.0" encoding="UTF-8"?>
+        //<S:Envelope xmlns:S="http://schemas.xmlsoap.org/soap/envelope/"><S:Body>
         final String prefix = "<S:Envelope xmlns:S=\"http://schemas.xmlsoap.org/soap/envelope/\"><S:Body>";
+        // <S:Envelope xmlns:S="http://schemas.xmlsoap.org/soap/envelope/"><S:Body>
         final String suffix = "</S:Body></S:Envelope>";
+        // </S:Body></S:Envelope>
         if (content.startsWith(prefix)) {
             content = content.substring(prefix.length());
         }
         if (content.endsWith(suffix)) {
             content = content.substring(0,content.length()-suffix.length());
         }
+        logger.trace("exit removeSoapEnvelope, content={}",content);
         return content;
     }
 }
